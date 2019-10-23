f1bfab11194b66a5520a65af9ccf259bf339ffa6
Akis Papadopoulos <iakopap@gmail.com>
2019-10-23 19:14:44 +0300

    Add a script to install the development stack.

---
+++ install-development-stack.sh
@@ -0,0 +1,355 @@
+#!/bin/bash
+# A shell script to install a development environment
+
+# Style markers
+R="\033[0m" # Reset styles
+V="\e[93m" # Highlight values in yellow
+S="\e[92m" # Highlight logs in green
+
+# Welcome screen
+echo -e "Welcome to the workspace installation process."
+
+echo -e "Date: ${V}$(date)${R}"
+echo -e "System: ${V}$(lsb_release -si) $(lsb_release -sr)${R}"
+echo -e "Host: ${V}$HOSTNAME${R}"
+echo -e "Username: ${V}$USER${R}\n"
+
+# Temporary folder
+read -p "Where do you want to save temporary files?(./.tmp)" path
+
+if [[ $path != "" ]]; then
+ temp=$path
+else
+ temp="./.tmp"
+fi
+
+if [[ -d $temp ]]; then
+ echo -e "Temporary folder ${V}$temp${R} already exists."
+else
+ echo -e "Creating temporary folder ${V}$temp${R}."
+ mkdir -p $temp
+fi
+
+echo -e "${S}Temporary folder has been set to $temp successfully.${R}\n"
+
+# Workspace folder
+read -p "Enter the absolute path to the workspace home folder:(home/$USER/Workspace) " path
+
+if [[ $path == "" ]]; then
+ path="/home/$USER/Workspace"
+fi
+
+if [[ -d $path ]]; then
+ echo -e "Path ${V}$path${R} already exists."
+else
+ echo -e "Creating path ${V}$path${R}."
+ mkdir -p $path
+fi
+
+echo -e "${S}Workspace home path has been set to $path successfully.${R}\n"
+
+# System upgrade
+read -p "Do you want to upgrade your base system to the latest updates?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Updating the repositories package list."
+  sudo apt update
+  sudo apt upgrade
+  echo -e "${S}System upgrade finished successfully.${R}\n"
+esac
+
+# Utility software
+read -p "Do you want to install third-party utilities?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Installing various software utilities."
+  sudo apt install tree curl
+  echo -e "${S}Software utilities have been installed successfully.${R}\n"
+esac
+
+# Chrome
+read -p "Do you want to install chrome?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Downloading the latest version of chrome."
+  url="https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb"
+  wget -q --show-progress -P $temp $url
+
+  echo -e "Installing chrome using deb packaging."
+  sudo dpkg -i $temp/google-chrome-stable_current_amd64.deb
+
+  echo -e "${S}Chrome has been installed successfully.${R}\n"
+esac
+
+# Skype
+read -p "Do you want to install skype?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Downloading the latest version of skype."
+  url="https://repo.skype.com/latest/skypeforlinux-64.deb"
+  wget -q --show-progress -P $temp $url
+
+  echo -e "Installing skype using deb packaging."
+  sudo dpkg -i $temp/skypeforlinux-64.deb
+
+  echo -e "${S}Skype has been installed successfully.${R}\n"
+esac
+
+# Slack
+read -p "Do you want to install slack?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+
+  read -p "Enter the url to the slack binary file: " url
+  echo -e "Downloading the latest version of slack."
+  wget -q --show-progress -P $temp -O $temp/slack-desktop-amd64.deb $url
+
+  echo -e "Installing slack using deb packaging."
+  sudo apt install $temp/slack-desktop-amd64.deb
+
+  echo -e "${S}Slack has been installed successfully.${R}\n"
+esac
+
+# Virtual Box v6.0
+read -p "Do you want to install virtual box 6.0?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Installing virtual box ${V}version 6.0${R}."
+  wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+  sudo apt update
+  sudo apt install virtualbox-6.0
+
+  echo -e "${S}Virtual box has been installed successfully.${R}\n"
+esac
+
+# Git
+read -p "Do you want to install git?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  ppa="git-core/ppa"
+
+  if ! grep -q "^deb .*$ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+   echo -e "Adding the git ppa repository."
+   sudo add-apt-repository ppa:$ppa
+   sudo apt update
+  fi
+
+  echo -e "Installing the latest version of git."
+  sudo apt install git
+
+  read -p "Enter a global username to be associated in each git commit:($USER)" username
+  if [[ $username == "" ]]; then
+   username = $USER
+  fi
+
+  git config --global user.name "$username"
+  echo -e "Global username has been set to ${V}$(git config --global user.name)${R}."
+
+  read -p "Enter a global email to be associated in each git commit:($USER@$HOSTNAME)" email
+  if [[ $email == "" ]]; then
+   email = $USER@$HOSTNAME
+  fi
+
+  git config --global user.email "$email"
+  echo -e "Global email has been set to ${V}$(git config --global user.email)${R}."
+
+  echo -e "${S}Git has been installed successfully.${R}\n"
+esac
+
+# JDK
+read -p "Do you want to install a JDK?(Y/n)" answer
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  javapath=$path/java
+  mkdir -p $javapath
+
+  read -p "Enter the url to the JDK binary tar.gz file: " url
+  echo -e "Downloading the JDK binary file."
+  wget -q --show-progress -P $temp --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" $url
+
+  echo -e "Extracting JDK binary files to ${V}$javapath${R}."
+  tar zxf $temp/jdk* -C $javapath
+  rm -rf $temp/jdk*
+
+  echo -e "${S}JDK has been installed succefully to $javapath.${R}\n"
+
+  # Install more JDKs
+  while :
+  do
+    echo -e "Currently installed JDKs are:"
+    tree -d --noreport -n -L 1 $javapath
+
+    read -p "Do you want to install another JDK?(Y/n)" answer
+
+    case $answer in
+     ( [Yy][Ee][Ss] | [Yy] | "" )
+      read -p "Enter the url to the JDK binary tar.gz file: " url
+      echo -e "Downloading the JDK binary file."
+      wget -q --show-progress -P $temp --no-check-certificate -c --header "Cookie: oraclelicense=accept-securebackup-cookie" $url
+
+      echo -e "Extracting JDK binary files to ${V}$javapath${R}."
+      tar zxf $temp/jdk* -C $javapath
+      rm -rf $temp/jdk*
+
+      echo -e "${S}JDK has been installed succefully to $javapath.${R}\n"
+      ;;
+     *)
+      break
+    esac
+  done
+
+  # Installing JDKs executables with update-alternatives
+  echo -e "Adding java and javac to update-alternatives."
+
+  for d in $javapath/* ; do
+    read -p "Found JDK $(basename $d), do you want to add it to update-alternatives?(Y/n)" answer
+
+    case $answer in
+     ( [Yy][Ee][Ss] | [Yy] | "" )
+      read -p "Enter the priority for this alternative entry: " priority
+
+      sudo update-alternatives --install /usr/bin/java java $d/bin/java $priority
+      sudo update-alternatives --install /usr/bin/javac javac $d/bin/javac $priority
+
+      echo -e "${S}JDK $(basename $d) has been added in alternatives.${R}\n"
+    esac
+  done
+esac
+
+# Maven
+read -p "Do you want to install maven?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  mavenpath=$path/maven
+  mkdir -p $mavenpath
+
+  read -p "Enter the url to the maven binary tar.gz file: " url
+  echo -e "Downloading the maven binary file."
+  wget -q --show-progress -P $temp $url
+
+  echo -e "Extracting the maven files to ${V}$mavenpath${R}."
+  tar zxf $temp/apache-maven* -C $mavenpath
+  rm -rf $temp/apache-maven*
+
+  echo -e "${S}Maven has been installed successfully in $mavenpath.${R}\n"
+
+  for d in $mavenpath/* ; do
+    read -p "Found Maven $(basename $d), do you want to add it to update-alternatives?(Y/n)" answer
+
+    case $answer in
+     ( [Yy][Ee][Ss] | [Yy] | "" )
+      read -p "Enter the priority for this alternative entry: " priority
+
+      sudo update-alternatives --install /usr/bin/mvn mvn $d/bin/mvn $priority
+
+      echo -e "${S}Maven $(basename $d) has been added in alternatives.${R}\n"
+    esac
+  done
+esac
+
+# Atom
+read -p "Do you want to install atom?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  echo -e "Installing the latest version of atom."
+  wget -q https://packagecloud.io/AtomEditor/atom/gpgkey -O- | sudo apt-key add -
+  sudo add-apt-repository "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main"
+  sudo apt update
+  sudo apt install atom
+
+  echo -e "${S}Atom has been installed successfully.${R}\n"
+esac
+
+# IdeaIC
+read -p "Do you want to install ideaIC?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  editorspath=$path/editors
+  mkdir -p $editorspath
+
+  read -p "Enter the url to the ideaIC tar.gz file: " url
+  echo -e "Downloading the ideaIC tar.gz file."
+  wget -q --show-progress -P $temp $url
+
+  echo -e "Extracting the ideaIC files to ${V}$editorspath${R}."
+  tar zxf $temp/ideaIC* -C $editorspath
+  rm -rf $temp/ideaIC*
+
+  echo -e "${S}IdeaIC has been installed successfully in $editorspath.${R}\n"
+esac
+
+# DBeaver
+read -p "Do you want to install dbeaver?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  editorspath=$path/editors
+  mkdir -p $editorspath
+
+  echo -e "Downloading the latest version of the dbeaver tar.gz file."
+  wget -q --show-progress -P $temp wget https://dbeaver.io/files/dbeaver-ce-latest-linux.gtk.x86_64.tar.gz
+
+  echo -e "Extracting the dbeaver files to ${V}$editorspath${R}."
+  tar zxf $temp/dbeaver-ce* -C $editorspath
+  rm -rf $temp/dbeaver-ce*
+
+  sudo ln -sfn $editorspath/dbeaver/dbeaver /usr/local/bin/dbeaver
+
+  echo -e "Creating dbeaver's application dock entry."
+
+  desktopfile="/usr/share/applications/dbeaver.desktop"
+  sudo touch $desktopfile
+  sudo echo "[Desktop Entry]" | sudo tee -a $desktopfile
+  sudo echo "Type=Application" | sudo tee -a $desktopfile
+  sudo echo "Name=DBeaver Community" | sudo tee -a $desktopfile
+  sudo echo "Icon=$editorspath/dbeaver/dbeaver.png" | sudo tee -a $desktopfile
+  sudo echo "Exec=$editorspath/dbeaver/dbeaver" | sudo tee -a $desktopfile
+  sudo echo "Comment=DBeaver Community" | sudo tee -a $desktopfile
+  sudo echo "Categories=Development;Databases;" | sudo tee -a $desktopfile
+
+  echo -e "${S}DBeaver has been installed successfully in the $editorspath.${R}\n"
+esac
+
+# Postman
+read -p "Do you want to install postman?(Y/n)" answer
+
+case $answer in
+ ( [Yy][Ee][Ss] | [Yy] | "" )
+  editorspath=$path/editors
+  mkdir -p $editorspath
+
+  echo -e "Downloading the latest version of postman."
+  url="https://dl.pstmn.io/download/latest/linux64"
+  wget -q --show-progress -P $temp -O $temp/postman.tar.gz $url
+
+  echo -e "Extracting postman files to ${V}$editorspath${R}."
+  tar zxf $temp/postman.tar.gz -C $editorspath
+  sudo ln -sfn $editorspath/Postman/Postman /usr/local/bin/postman
+
+  echo -e "Creating postman's application dock entry."
+
+  desktopfile="/usr/share/applications/postman.desktop"
+  sudo touch $desktopfile
+  sudo echo "[Desktop Entry]" | sudo tee -a $desktopfile
+  sudo echo "Type=Application" | sudo tee -a $desktopfile
+  sudo echo "Name=Postman" | sudo tee -a $desktopfile
+  sudo echo "Icon=$editorspath/Postman/app/resources/app/assets/icon.png" | sudo tee -a $desktopfile
+  sudo echo "Exec=$editorspath/Postman/Postman" | sudo tee -a $desktopfile
+  sudo echo "Comment=Postman" | sudo tee -a $desktopfile
+  sudo echo "Categories=Development;Code;" | sudo tee -a $desktopfile
+
+  echo -e "${S}Postman has been installed successfully in $editorspath.${R}\n"
+esac
+
+echo -e "Installation completed successfully."
