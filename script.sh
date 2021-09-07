f87303123605f2c47ef4d26c88d6b9e85f935856
Jake Ob <iakopap@gmail.com>
2021-09-07 20:19:12 +0300

    Fix broken docker apt repository key

--- script.sh
+++ script.sh
@@ -237,11 +237,11 @@ installDocker () {
 
   log "Adding docker repository to apt sources..."
 
-  curl -fsSL https://download.docker.com/linux/ubuntu/gpg >> $LOG_FILE 2>&1 | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >> $LOG_FILE 2>&1
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg >> $LOG_FILE 2>&1
   
   echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >> $LOG_FILE 2>&1
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
   log "Docker apt repository has been added to sources"
 
