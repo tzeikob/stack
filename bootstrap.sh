2a7eb9ef5ed0a858e5b7716e44d41e77f018e1e5
Jake Ob <iakopap@gmail.com>
2022-05-16 11:04:30 +0300

    Ask the device path to the installation disk

--- bootstrap.sh
+++ bootstrap.sh
@@ -18,3 +18,15 @@ echo -e "Starting base installation process"
 if [ ! -d "/sys/firmware/efi/efivars" ]; then
   abort "This script supports only UEFI systems" 1
 fi
+
+echo -e "\nPartitioning and formatting the installation disk..."
+echo -e "The following disks found in your system:"
+
+lsblk
+
+read -p "Enter the device path of the disk to apply the installation: " device
+
+while [ ! -b "$device" ]; do
+  echo -e "Invalid device path: '$device'"
+  read -p "Please enter a valid device path: " device
+done
