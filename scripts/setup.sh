9a53bc9d3e78c60063098f99b075f526bdb377b0
Jake Ob <iakopap@gmail.com>
2022-08-25 17:39:35 +0300

    Setup the keymap, locale and timezone

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,4 +2,22 @@
 
 source $OPTIONS
 
-echo "TODO: Start setting things up, like users, mirrors, drivers etc."
+echo -e "\nStarting the setup process..."
+
+echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
+loadkeys $KEYMAP
+
+echo "Keyboard's keymap has been set to $KEYMAP"
+
+ln -sf /usr/share/zoneinfo/$TIMEZONE /etc/localtime
+
+echo "Local timezone has been set to $TIMEZONE"
+
+echo "LANG=$LOCALE" >> /etc/locale.conf
+sed -i "s/#\(${LOCALE}.*\)/\1/" /etc/locale.gen
+locale-gen
+
+echo "Locale has been set to $LOCALE"
+
+echo "Moving to the next process..."
+sleep 5
