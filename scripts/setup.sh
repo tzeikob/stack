c96692bb5957f1483dfcd819e22c93ee50270398
Jake Ob <iakopap@gmail.com>
2022-08-25 18:32:47 +0300

    Setting up root and sudoer user

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,7 +2,19 @@
 
 source $OPTIONS
 
+nopasswd_on () {
+  sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+  echo "No password mode has been enabled"
+}
+
+nopasswd_off () {
+  sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+  echo "No password mode has been disabled"
+}
+
 echo -e "\nStarting the setup process..."
+
+nopasswd_on
 
 echo "KEYMAP=$KEYMAP" > /etc/vconsole.conf
 loadkeys $KEYMAP
@@ -33,6 +45,21 @@ echo "::1          localhost" >> /etc/hosts
 echo "127.0.1.1    $HOSTNAME" >> /etc/hosts
 
 echo -e "Hostname has been set to $HOSTNAME"
+
+useradd -m -G wheel,audio,video,optical,storage $USERNAME
+sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+echo "Sudoer user $USERNAME has been created"
+
+echo "root:$ROOT_PASSWORD" | chpasswd
+
+echo "User root has been given a password"
+
+echo "$USERNAME:$USER_PASSWORD" | chpasswd
+
+echo "User $USERNAME have been given a password"
+
+nopasswd_off
 
 echo "Moving to the next process..."
 sleep 5
