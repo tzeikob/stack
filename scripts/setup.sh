94ae0c671c489d9b01e68bf1248f83d013983f94
Jake Ob <iakopap@gmail.com>
2022-08-30 20:16:57 +0300

    Install yay package in noconfirm mode

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -154,7 +154,7 @@ install_yay () {
   git clone https://aur.archlinux.org/yay.git
 
   chown -R $USERNAME:$USERNAME yay && cd yay
-  sudo -u $USERNAME makepkg -si
+  sudo -u $USERNAME makepkg -si --noconfirm --needed
 
   cd /root && rm -rf /home/$USERNAME/yay
 
