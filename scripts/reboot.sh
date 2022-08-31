e9a4e5029b0e5b6d81f96faec596df8feedc58a2
Jake Ob <iakopap@gmail.com>
2022-08-31 18:18:03 +0300

    Keep code uniformity between all scripts

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,19 +1,29 @@
 #!/usr/bin/env bash
 
+copy_files () {
+  echo "Copying all installation files to the new system"
+
+  rm -rf /mnt/root/stack
+  cp -R $HOME /mnt/root/
+
+  echo "Installation files have been copied successfully"
+}
+
+unmount () {
+  umount -R /mnt &&
+    echo "Partitions under /mnt have been unmounted" ||
+    echo "Ignoring any busy mount points"
+}
+
+restart () {
+  echo "Rebooting the system in 15 secs (ctrl-c to skip)..."
+
+  sleep 15
+  reboot
+}
+
 echo -e "\nBooting into the system for the first time..."
 
-echo "Copying all installation files to the new system"
-
-rm -rf /mnt/root/stack
-cp -R $HOME /mnt/root/
-
-echo "Installation files have been copied successfully"
-
-umount -R /mnt &&
-  echo "Partitions under /mnt have been unmounted" ||
-  echo "Ignoring any busy mount points"
-
-echo "Rebooting the system in 15 secs (ctrl-c to skip)..."
-
-sleep 15
-reboot
+copy_files &&
+  unmount &&
+  restart
