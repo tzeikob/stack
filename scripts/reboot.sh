85d5698b405f9ec1258d59b76f0719c166e4f59c
Jake Ob <iakopap@gmail.com>
2022-08-31 10:24:55 +0300

    Copying installation files and logs before the first boot

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -2,11 +2,18 @@
 
 echo "Booting into the system for the first time..."
 
+echo "Copying all installation files to the new system"
+
+rm -rf /mnt/root/stack
+cp -R $HOME /mnt/root/
+
+echo "Installation files have been copied successfully"
+
 umount -R /mnt &&
   echo "Partitions under /mnt have been unmounted" ||
-  echo "Ignoring some busy mount points"
+  echo "Ignoring any busy mount points"
 
-echo -e "\nRebooting the system in 15 secs (ctrl-c to skip)..."
+echo "Rebooting the system in 15 secs (ctrl-c to skip)..."
 
 sleep 15
 reboot
