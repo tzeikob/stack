fb2bf63cdb357e4b3a38058668ed873eaaed1707
Jake Ob <iakopap@gmail.com>
2022-09-13 20:17:09 +0300

    Fix broken append command in xorg configuration

--- desktop/install.sh
+++ desktop/install.sh
@@ -346,7 +346,7 @@ config_xorg () {
   echo 'udiskie --notify-command "ln -s /run/media/$USER $HOME/media/local" &' >> ~/.xinitrc
   echo "picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &" >> ~/.xinitrc
   echo "~/.config/feh/fehbg &" >> ~/.xinitrc
-  echo "xsetroot -cursor_name left_ptr" ~/.xinitrc
+  echo "xsetroot -cursor_name left_ptr" >> ~/.xinitrc
   echo "exec bspwm" >> ~/.xinitrc
 
   echo "Compositor and window manager launchers added successfully"
