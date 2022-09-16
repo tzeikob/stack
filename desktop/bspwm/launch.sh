9fc755871f17ca3e5d8e9edde6cbd9c15b952de0
Jake Ob <iakopap@gmail.com>
2022-09-16 20:42:19 +0300

    Move calls not related to window manager to xinitrc

--- desktop/bspwm/launch.sh
+++ desktop/bspwm/launch.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-xsetroot -cursor_name left_ptr
-
-udiskie --notify-command "ln -s /run/media/$USER $HOME/media/local" &
 picom --fade-in-step=1 --fade-out-step=1 --fade-delta=0 &
 ~/.config/feh/fehbg &
 exec bspwm
