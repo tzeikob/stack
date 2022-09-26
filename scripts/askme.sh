1a17fdd7e0ba864c1cbb51f608d3ef9f633be914
Jake Ob <iakopap@gmail.com>
2022-09-26 22:30:06 +0300

    Fix broken task flow in askme script

--- scripts/askme.sh
+++ scripts/askme.sh
@@ -690,7 +690,7 @@ while true; do
     what_password "USER" &&
     what_password "ROOT" &&
     which_kernels &&
-    opt_in "editors" "code" "atom" "sublime" "neovim"
+    opt_in "editors" "code" "atom" "sublime" "neovim" &&
     opt_in "browsers" "firefox" "chrome" "brave" "tor"
 
   echo "Configuration options have been set to:"
