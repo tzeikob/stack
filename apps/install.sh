7ee616e506199a7176df43962d1bc69e864742fd
Jake Ob <iakopap@gmail.com>
2022-10-08 21:28:36 +0300

    Extend mount nnn plugin to sync google drive via rclone

--- apps/install.sh
+++ apps/install.sh
@@ -442,7 +442,7 @@ install_filezilla () {
 install_rclone () {
   echo "Installing the RClone... "
 
-  sudo pacman -S --noconfirm rclone || exit 1
+  sudo pacman -S --noconfirm fuse2 rclone || exit 1
 
   echo -e "RClone has been installed\n"
 }
