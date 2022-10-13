f3969e4105ff8bec38c375fae4287554aac64ea3
Jake Ob <iakopap@gmail.com>
2022-10-13 12:34:55 +0300

    Install utility to pick colors from screen

--- system/install.sh
+++ system/install.sh
@@ -176,7 +176,7 @@ install_packages () {
     man-db man-pages texinfo cups bluez bluez-utils unzip terminus-font \
     vim nano git htop tree arch-audit atool zip xz unace p7zip gzip lzop feh \
     bzip2 unrar dialog inetutils dnsutils openssh nfs-utils openbsd-netcat ipset \
-    neofetch age polkit-gnome imagemagick \
+    neofetch age polkit-gnome imagemagick gpick \
     $([ "$UEFI" = "yes" ] && echo 'efibootmgr') || exit 1
 
   echo -e "\nReplacing iptables with nft tables..."
