2338f381f790070f1044b049d97d9684df2aaae5
Jake Ob <iakopap@gmail.com>
2022-11-10 20:31:16 +0200

    Move launcher script under rofi home folder

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -170,6 +170,8 @@ install_launchers () {
   cp ~/stack/resources/rofi/config.rasi "$CONFIG_HOME"
   chmod 644 "$CONFIG_HOME/config.rasi"
 
+  cp ~/stack/resources/rofi/launch "$CONFIG_HOME"
+
   echo "Launchers has been installed"
 }
 
--- scripts/system.sh
+++ scripts/system.sh
@@ -293,7 +293,6 @@ install_utilities () {
   cp ~/stack/resources/stack/reboot "$STACK_HOME"
   cp ~/stack/resources/stack/logout "$STACK_HOME"
   cp ~/stack/resources/stack/lock "$STACK_HOME"
-  cp ~/stack/resources/stack/launch "$STACK_HOME"
   cp ~/stack/resources/stack/networks "$STACK_HOME"
 
   ln -sf "$STACK_HOME/mount" /usr/local/bin/mmn
@@ -302,7 +301,6 @@ install_utilities () {
   ln -sf "$STACK_HOME/reboot" /usr/local/bin/rrb
   ln -sf "$STACK_HOME/logout" /usr/local/bin/llo
   ln -sf "$STACK_HOME/lock" /usr/local/bin/llk
-  ln -sf "$STACK_HOME/launch" /usr/local/bin/launch
   ln -sf "$STACK_HOME/networks" /usr/local/bin/networks
 
   echo "Stack utilities have been installed"
