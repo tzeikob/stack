d2a85067cf6f24999de5630981e1798af5e6c53a
Jake Ob <iakopap@gmail.com>
2023-01-31 16:04:57 +0200

    Improve overall config and resources organization for all stack utils
    This commit fixes:
    * store configurations and settings in single json file
    * all resource files like wallpapers, fonts etc should go under .local/share

--- scripts/desktop.sh
+++ scripts/desktop.sh
@@ -356,12 +356,13 @@ install_theme () {
 
   cp ~/stack/resources/gtk/settings.ini "$GTK_HOME"
 
-  local CONFIG_HOME=~/.config/stack/theme
-  local WALLPAPERS_HOME="$CONFIG_HOME/wallpapers"
+  local WALLPAPERS=~/.local/share/wallpapers
+  mkdir -p "$WALLPAPERS"
+  cp ~/stack/resources/feh/wallpaper.jpeg "$WALLPAPERS/default.jpeg"
 
-  mkdir -p "$WALLPAPERS_HOME"
-  cp ~/stack/resources/feh/wallpaper.jpeg "$WALLPAPERS_HOME/default.jpeg"
-  echo '{"wallpaper": {"file": "'$WALLPAPERS_HOME/default.jpeg'", "mode": "fill"}}' > "$CONFIG_HOME/.settings"
+  local CONFIG_HOME=~/.config/stack
+  mkdir -p "$CONFIG_HOME"
+  echo '{"theme": {"wallpaper": {"file": "default.jpeg", "mode": "fill"}}}' > "$CONFIG_HOME/.settings"
 
   echo "Default wallpaper has been set"
   echo "Theme has been setup"
