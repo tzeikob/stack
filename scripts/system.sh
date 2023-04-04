e33a2a60b77af7e31bacdeaeb8fedb22ab72df24
Jake Ob <iakopap@gmail.com>
2023-04-04 11:38:49 +0300

    Install packages required for the gio command

--- scripts/system.sh
+++ scripts/system.sh
@@ -188,7 +188,7 @@ install_packages () {
     man-db man-pages texinfo cups bluez bluez-utils unzip terminus-font \
     vim nano git tree arch-audit atool zip xz unace p7zip gzip lzop feh \
     bzip2 unrar dialog inetutils dnsutils openssh nfs-utils openbsd-netcat ipset \
-    neofetch age imagemagick gpick fuse2 rclone smartmontools jq jc \
+    neofetch age imagemagick gpick fuse2 rclone smartmontools glib2 jq jc \
     $([ "$UEFI" = "yes" ] && echo 'efibootmgr') || exit 1
 
   echo -e "\nReplacing iptables with nft tables..."
