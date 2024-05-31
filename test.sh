a93f0c81a389e345cd4605e3cfa48c3fe7d6c802
Jake Ob <iakopap@gmail.com>
2024-05-31 15:07:30 +0300

    Add more tests units

--- test.sh
+++ test.sh
@@ -33,16 +33,82 @@ abort () {
 
 # Asserts no other than shell files exist under the src folder.
 test_no_shell_files () {
-  local files=($(find ./src -type f -not -name '*.sh'))
+  local count=0
+  count=$(find ./src -type f -not -name '*.sh' | wc -l) ||
+    abort ERROR 'Unable to list source files.'
 
-  if [[ ${#files[@]} -gt 0 ]]; then
-    log ERROR '[FAILED] No shell files test'
+  if [[ ${count} -gt 0 ]]; then
+    log ERROR '[FAILED] No shell files test.'
     return 1
   fi
 
-  log INFO '[PASSED] No shell files test'
+  log INFO '[PASSED] No shell files test.'
+}
+
+# Asserts all func names have a name with lower letters and underscores.
+test_valid_func_names () {
+  local valid_declaration='^[a-zA-Z_0-9]{1,} \(\) \{$'
+
+  local files=''
+  files=($(find ./src -type f -name '*.sh')) ||
+    abort ERROR 'Unable to list source files.'
+
+  local file=''
+  for file in "${files[@]}"; do
+    local funcs=''
+    funcs=$(grep '.*( *) *{.*' ${file}) ||
+      abort ERROR 'Unable to read function declarations.'
+
+    local func=''
+    while read -r func; do
+      if [[ ! "${func}" =~ ${valid_declaration} ]]; then
+        log ERROR '[FAILED] Valid func names test.'
+        return 1
+      fi
+    done <<< "${funcs}"
+  done
+
+  log INFO '[PASSED] Valid func names test.'
+}
+
+# Asserts no func gets overriden by any other func under src.
+test_no_func_overriden () {
+  local files=''
+  files=($(find ./src -type f -name '*.sh')) ||
+    abort ERROR 'Unable to list source files.'
+
+  local total_funcs=''
+
+  local file=''
+  for file in "${files[@]}"; do
+    local funcs=''
+    funcs=$(grep '.*( *) *{.*' ${file} | cut -d ' ' -f 1) ||
+      abort ERROR 'Unable to read function declarations.'
+
+    total_funcs+=$'\n'"${funcs}"
+  done
+
+  local func=''
+  while read -r func; do
+    if [[ -z "${func}" ]] || [[ "${func}" =~ '^ *$' ]]; then
+      continue
+    fi
+
+    local occurrences=0
+    occurrences=$(echo "${total_funcs}" | grep "^${func}$" | wc -l) ||
+      abort ERROR 'Unable to iterate through function declarations.'
+
+    if [[ ${occurrences} -gt 1 ]]; then
+      log ERROR '[FAILED] No func overriden test'
+      return 1
+    fi
+  done <<< "${total_funcs}"
+
+  log INFO '[PASSED] No func overriden test'
 }
 
 test_no_shell_files &&
+  test_valid_func_names &&
+  test_no_func_overriden &&
   log INFO 'All test assertions have been passed.' ||
   abort ERROR 'Some tests have been failed to pass.'
