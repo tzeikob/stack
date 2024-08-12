c82dc608a1d02342bc18121916103b2962e74545
Jake Ob <iakopap@gmail.com>
2024-08-12 20:07:20 +0300

    Use a single rsync cmd to upgrade all root files

--- upgrade.sh
+++ upgrade.sh
@@ -6,6 +6,25 @@ source src/commons/error.sh
 source src/commons/logger.sh
 source src/commons/validators.sh
 
+# Updates the root file system.
+update_root_files () {
+  log INFO 'Updating the root file system...'
+
+  mv airootfs/home/user "airootfs/home/${USER}" ||
+    abort ERROR "Failed to prepare home folder for ${USER}."
+
+  sudo rsync -av airootfs/ / \
+    --exclude etc/X11/xorg.conf.d/00-keyboard.conf \
+    --exclude etc/hostname \
+    --exclude etc/hosts \
+    --exclude etc/vconsole.conf \
+    --exclude "home/${USER}/.config/gtk-3.0" \
+    --exclude "home/${USER}/.config/stack" ||
+    abort ERROR 'Failed to update the root file system.'
+  
+  log INFO 'Root file system has been updated.'
+}
+
 # Updates the commons script files.
 update_commons () {
   log INFO 'Updating the commons files...'
@@ -24,8 +43,7 @@ update_commons () {
 update_tools () {
   log INFO 'Updating the tools files...'
 
-  sudo rsync -av --delete src/tools/ /opt/stack/tools &&
-    sudo rsync -av --exclude tqdm airootfs/usr/local/bin/ /usr/local/bin ||
+  sudo rsync -av --delete src/tools/ /opt/stack/tools ||
     abort ERROR 'Failed to update the tools files.'
   
   sudo sed -i 's;source src;source /opt/stack;' /opt/stack/tools/**/* ||
@@ -42,89 +60,12 @@ update_release_data () {
   local version=''
   version="$(date +%Y.%m.%d)" ||
     abort ERROR 'Failed to create release version.'
-
-  sudo cp airootfs/usr/lib/os-release /usr/lib/os-release ||
-    abort ERROR 'Failed to update the release file.'
-  
-  log INFO 'Release file has been updated.'
   
   sudo sed -i "s/#VERSION#/${version}/" /usr/lib/os-release ||
     abort ERROR 'Failed to set release version.'
   
   log INFO "New release version set to ${version}."
-  
-  sudo cp airootfs/etc/stack-release /etc/stack-release ||
-    abort ERROR 'Failed to update the stack-release symlink.'
-  
-  log INFO 'Stack release symlink has been updated.'
-
-  sudo rm -f /etc/arch-release ||
-    abort ERROR 'Unable to remove the arch-release file.'
-  
   log INFO 'Release data have been updated.'
-}
-
-# Updates the pacman hooks and scripts.
-update_pacman_hooks () {
-  log INFO 'Updating pacman hooks and scripts...'
-
-  sudo rsync -av airootfs/etc/pacman.d/hooks/ /etc/pacman.d/hooks ||
-    abort ERROR 'Failed to update pacman hooks.'
-  
-  log INFO 'Pacman hooks have been updated.'
-  
-  sudo rsync -av airootfs/etc/pacman.d/scripts/ /etc/pacman.d/scripts ||
-    abort ERROR 'Failed to update pacman scripts.'
-  
-  log INFO 'Pacman scripts have been updated.'
-}
-
-# Updates proxy sudoers rules.
-update_proxy_rules () {
-  log INFO 'Updating sudoers proxy rules...'
-
-  sudo rsync -av \
-    airootfs/etc/sudoers.d/proxy_rules /etc/sudoers.d/proxy_rules ||
-    abort ERROR 'Failed to update sudoers proxy rules.'
-  
-  log INFO 'Sudoers proxy rules have been updated.'
-}
-
-# Updates power configurations.
-update_power_configurations () {
-  log INFO 'Updating power configurations...'
-
-  sudo rsync -av \
-    airootfs/etc/systemd/logind.conf.d/ /etc/systemd/logind.conf.d ||
-    abort ERROR 'Failed to update login power settings.'
-  
-  log INFO 'Login power settings have been updated.'
-
-  sudo rsync -av \
-    airootfs/etc/systemd/sleep.conf.d/ /etc/systemd/sleep.conf.d ||
-    abort ERROR 'Failed to update sleep power settings.'
-  
-  log INFO 'Sleep power settings have been updated.'
-  
-  sudo rsync -av airootfs/etc/tlp.d/ /etc/tlp.d ||
-    abort ERROR 'Failed to update TLP power settings.'
-  
-  log INFO 'TLP power settings have been updated.'
-  
-  sudo rsync -av airootfs/etc/x11/xorg.conf /etc/x11/xorg.conf ||
-    abort ERROR 'Failed to update the xorg configuration.'
-  
-  log INFO 'Xorg configuration has been updated.'
-}
-
-# Updates devices rules.
-update_devices_rules () {
-  log INFO 'Updating devices rules...'
-
-  sudo rsync -av airootfs/etc/udev/rules.d/ /etc/udev/rules.d ||
-    abort ERROR 'Failed to update devices rules.'
-  
-  log INFO 'Devices rules have been updated.'
 }
 
 # Updates the locker.
@@ -133,12 +74,6 @@ update_locker () {
 
   sudo systemctl disable lock@${USER}.service 2>&1 ||
     log WARN 'Unable to disable lock service.'
-
-  sudo rsync -av \
-    airootfs/etc/systemd/system/lock@.service /etc/systemd/system/lock@.service ||
-    abort ERROR 'Failed to update the lock service.'
-  
-  log INFO 'Lock service has been updated.'
   
   local user_id=''
   user_id="$(
@@ -147,13 +82,7 @@ update_locker () {
 
   sudo sed -i "s/#USER_ID#/${user_id}/g" /etc/systemd/system/lock@.service &&
     abort ERROR 'Failed to set the user id in lock service.'
-  
-  sudo rsync -av \
-    airootfs/usr/lib/systemd/system-sleep/locker /usr/lib/systemd/system-sleep/locker ||
-    abort ERROR 'Failed to update the lock service hook.'
-  
-  log INFO 'Lock service hook has been updated.'
-  
+
   sudo systemctl enable lock@${USER}.service 2>&1 ||
     log WARN 'Unable to enable lock service.'
   
@@ -161,27 +90,9 @@ update_locker () {
   log INFO 'Locker has been updated.'
 }
 
-# Updates the tqdm patch file.
-update_tqdm_patch () {
-  log INFO 'Updating the tqdm patch file...'
-
-  sudo rsync -av airootfs/usr/local/bin/tqdm /usr/local/bin/tqdm ||
-    abort ERROR 'Failed to update the tqdm patch file.'
-
-  log INFO 'Tqdm patch file has been updated.'
-}
-
-# Updates the desktop files and modules.
-update_desktop () {
-  log INFO 'Updating the desktop files...'
-
-  rsync -av airootfs/home/user/.xinitrc "/home/${USER}/.xinitrc" ||
-    abort ERROR 'Failed to update the .xinitrc file.'
-  
-  log INFO 'Xinitrc file has been updated.'
-
-  rsync -av airootfs/home/user/.stackrc "/home/${USER}/.stackrc" ||
-    abort ERROR 'Failed to update the .stackrc file.'
+# Updates the default terminal and editor.
+update_default_terminal_and_editor () {
+  log INFO 'Updating the default terminal and editor...'
   
   sed -i \
     -e 's/#TERMINAL#/alacritty/' \
@@ -189,35 +100,11 @@ update_desktop () {
     abort ERROR 'Failed to set the terminal defaults.'
   
   log INFO 'Stackrc file has been updated.'
-
-  rsync -av --exclude stack --exclude gtk-3.0 --exclude systemd \
-    airootfs/home/user/.config/ "/home/${USER}/.config" ||
-    abort ERROR 'Failed to update the user config folder.'
-  
-  log INFO 'User config folder has been updated.'
-
-  rsync -av \
-    airootfs/home/user/.local/share/wallpapers/ "/home/${USER}/.local/share/wallpapers" ||
-    abort ERROR 'Failed to update the wallpapers.'
-  
-  log INFO 'Wallpapers have been updated.'
-
-  sudo rsync -av airootfs/usr/share/sounds/system/ /usr/share/sounds/system ||
-    abort ERROR 'Failed to update the system sounds.'
-  
-  log INFO 'System sounds have been updated.'
-  log INFO 'Desktop files have been updated.'
 }
 
 # Updates user and system services.
 update_services () {
   log INFO 'Updating user and system services...'
-
-  rsync -av \
-    airootfs/home/user/.config/systemd/user/ "/home/${USER}/.config/systemd/user" ||
-    abort ERROR 'Failed to update user services.'
-  
-  log INFO 'User services have been updated.'
   
   sed -i "s;#HOME#;/home/${USER};g" \
     "/home/${USER}/.config/systemd/user/fix-layout.service" ||
@@ -231,7 +118,7 @@ restore_user_permissions () {
   chown -R ${USER}:${USER} "/home/${USER}" ||
     abort ERROR 'Failed to restore user permissions.'
   
-  log INFO 'User permissions have been restored.'
+  log INFO "Permissions under /home/${user} restored."
 }
 
 # Updates the stack hash file.
@@ -257,16 +144,12 @@ fi
 
 log INFO 'Starting the upgrade process...'
 
-update_commons &&
+update_root_files &&
+  update_commons &&
   update_tools &&
   update_release_data &&
-  update_pacman_hooks &&
-  update_proxy_rules &&
-  update_power_configurations &&
-  update_devices_rules &&
   update_locker &&
-  update_tqdm_patch &&
-  update_desktop &&
+  update_default_terminal_and_editor &&
   update_services &&
   restore_user_permissions &&
   update_hash_file
