733bbca6445da57a1eca1191b2b1a256d2ef543f
Jake Ob <iakopap@gmail.com>
2024-12-04 14:26:40 +0200

    Fix incorrect qalc package name

--- src/installer/bootstrap.sh
+++ src/installer/bootstrap.sh
@@ -124,7 +124,7 @@ install_kernel () {
 
   linux_pkgs+=' linux-firmware'
 
-  local util_pkgs='reflector rsync sudo jq qalc'
+  local util_pkgs='reflector rsync sudo jq libqalculate'
 
   pacstrap /mnt base ${linux_pkgs} ${util_pkgs} 2>&1 ||
     abort ERROR 'Failed to pacstrap kernel and base packages.'
