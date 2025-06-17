4a9192ab630deb39df1ca329c79ec22b964f70bd
tzeikob <iakopap@gmail.com>
2025-06-17 19:21:37 +0300

    Merge sdkits script into apps

--- src/installer/apps.sh
+++ src/installer/apps.sh
@@ -9,13 +9,141 @@ source src/commons/math.sh
 
 SETTINGS_FILE=./settings.json
 
-# Installs the google chrome web browser.
-install_chrome () {
-  log INFO 'Installing the chrome web browser...'
+# Installs the node javascript runtime engine.
+install_node () {
+  log INFO 'Installing the node runtime engine...'
 
-  yay -S --needed --noconfirm --removemake google-chrome 2>&1 &&
-    log INFO 'Chrome web browser has been installed.' ||
-    log WARN 'Failed to install the chrome web browser.'
+  local user_name=''
+  user_name="$(jq -cer '.user_name' "${SETTINGS_FILE}")" ||
+    abort ERROR 'Unable to read user_name setting.'
+
+  local nvm_home="/home/${user_name}/.nvm"
+
+  local previous_dir=${PWD}
+
+  git clone https://github.com/nvm-sh/nvm.git "${nvm_home}" 2>&1 &&
+    cd "${nvm_home}" &&
+    git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)` 2>&1 &&
+    cd "${previous_dir}"
+  
+  if has_failed; then
+    log WARN 'Failed to install node version manager.'
+    return 0
+  fi
+
+  log INFO 'Node version manager has been installed.'
+
+  local bashrc_file="/home/${user_name}/.bashrc"
+
+  local hooks=''
+  hooks+=$'\nexport NVM_DIR="${HOME}/.nvm"'
+  hooks+=$'\n[ -s "${NVM_DIR}/nvm.sh" ] && \. "${NVM_DIR}/nvm.sh"'
+  hooks+=$'\n[ -s "${NVM_DIR}/bash_completion" ] && \. "${NVM_DIR}/bash_completion"'
+  hooks+=$'\nexport PATH="./node_modules/.bin:${PATH}"'
+
+  echo "${hooks}" >> "${bashrc_file}" ||
+    log WARN 'Failed to add node hooks to bashrc.'
+
+  log INFO 'Installing the latest node version...'
+
+  \. "${nvm_home}/nvm.sh" 2>&1 &&
+    nvm install --no-progress node 2>&1 ||
+    log WARN 'Failed to install the latest version of node.'
+
+  log INFO 'Node runtime engine has been installed.'
+}
+
+# Installs the go programming language.
+install_go () {
+  log INFO 'Installing the go programming language...'
+
+  sudo pacman -S --needed --noconfirm go go-tools 2>&1 &&
+    log INFO 'Go programming language has been installed.' ||
+    log WARN 'Failed to install go programming language.'
+}
+
+# Installs the rust programming language.
+install_rust () {
+  log INFO 'Installing the rust programming language...'
+
+  sudo pacman -S --needed --noconfirm rustup 2>&1
+  
+  if has_failed; then
+    log WARN 'Failed to install rust programming language.'
+    return 0
+  fi
+
+  log INFO 'Rustup has been installed.'
+
+  log INFO 'Setting the default tool chain...'
+
+  rustup default stable 2>&1 &&
+    log INFO 'Rust default tool chain set to stable.' ||
+    log WARN 'Failed to set default tool chain.'
+
+  log INFO 'Rust programming language has been installed.'
+}
+
+# Installs the docker engine.
+install_docker () {
+  log INFO 'Installing the docker engine...'
+
+  sudo pacman -S --needed --noconfirm docker docker-compose 2>&1
+  
+  if has_failed; then
+    log WARN 'Failed to install docker engine.'
+    return 0
+  fi
+
+  log INFO 'Docker packages have been installed.'
+
+  sudo systemctl enable docker.service 2>&1 &&
+    log INFO 'Docker service has been enabled.' ||
+    log WARN 'Failed to enable docker service.'
+
+  local user_name=''
+  user_name="$(jq -cer '.user_name' "${SETTINGS_FILE}")" ||
+    abort ERROR 'Unable to read user_name setting.'
+
+  sudo usermod -aG docker "${user_name}" 2>&1 &&
+    log INFO 'User added to the docker user group.' ||
+    log WARN 'Failed to add user to docker group.'
+
+  log INFO 'Docker egine has been installed.'
+}
+
+# Installs the virtual box.
+install_virtual_box () {
+  log INFO 'Installing the virtual box...'
+
+  local kernel=''
+  kernel="$(jq -cer '.kernel' "${SETTINGS_FILE}")" ||
+    abort ERROR 'Unable to read kernel setting.'
+
+  local pkgs='virtualbox virtualbox-guest-iso'
+
+  if equals "${kernel}" 'stable'; then
+    pkgs+=' virtualbox-host-modules-arch'
+  elif equals "${kernel}" 'lts'; then
+    pkgs+=' virtualbox-host-dkms'
+  fi
+
+  sudo pacman -S --needed --noconfirm ${pkgs} 2>&1
+
+  if has_failed; then
+    log WARN 'Failed to install virtual box.'
+    return 0
+  fi
+
+  local user_name=''
+  user_name="$(jq -cer '.user_name' "${SETTINGS_FILE}")" ||
+    abort ERROR 'Unable to read user_name setting.'
+
+  sudo usermod -aG vboxusers "${user_name}" 2>&1 &&
+    log INFO 'User added to the vboxusers user group.' ||
+    log WARN 'Failed to add user to vboxusers group.'
+
+  log INFO 'Virtual box has been installed.'
 }
 
 # Installs the postman client.
@@ -55,40 +183,6 @@ install_dbeaver () {
     log WARN 'Failed to install dbeaver client.'
 }
 
-# Installs the virtual box.
-install_virtual_box () {
-  log INFO 'Installing the virtual box...'
-
-  local kernel=''
-  kernel="$(jq -cer '.kernel' "${SETTINGS_FILE}")" ||
-    abort ERROR 'Unable to read kernel setting.'
-
-  local pkgs='virtualbox virtualbox-guest-iso'
-
-  if equals "${kernel}" 'stable'; then
-    pkgs+=' virtualbox-host-modules-arch'
-  elif equals "${kernel}" 'lts'; then
-    pkgs+=' virtualbox-host-dkms'
-  fi
-
-  sudo pacman -S --needed --noconfirm ${pkgs} 2>&1
-
-  if has_failed; then
-    log WARN 'Failed to install virtual box.'
-    return 0
-  fi
-
-  local user_name=''
-  user_name="$(jq -cer '.user_name' "${SETTINGS_FILE}")" ||
-    abort ERROR 'Unable to read user_name setting.'
-
-  sudo usermod -aG vboxusers "${user_name}" 2>&1 &&
-    log INFO 'User added to the vboxusers user group.' ||
-    log WARN 'Failed to add user to vboxusers group.'
-
-  log INFO 'Virtual box has been installed.'
-}
-
 # Prints dummy log lines to fake tqdm progress bar, when a
 # task gives less lines than it is expected to print and so
 # it resolves with fake lines to emulate completion.
@@ -117,12 +211,15 @@ if equals "$(id -u)" 0; then
   abort ERROR 'Script apps.sh must be run as non root user.'
 fi
 
-install_chrome &&
+install_node &&
+  install_go &&
+  install_rust &&
+  install_docker &&
+  install_virtual_box &&
   install_postman &&
   install_compass &&
   install_studio3t &&
-  install_dbeaver &&
-  install_virtual_box ||
+  install_dbeaver ||
   abort
 
 log INFO 'Script apps.sh has finished.'
